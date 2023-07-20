79833fa4486876651a6c5dd7ff92dc6f7d658525
Leonid Verhovskij <l.verhovskij@consolinno.de>
2023-07-20 15:47:32 +0200

    wip: pv data improved

--- integrationplugingenericconsolinno.cpp
+++ integrationplugingenericconsolinno.cpp
@@ -76,12 +76,15 @@ void IntegrationPluginGenericConsolinno::discoverThings(ThingDiscoveryInfo *info
 
 void IntegrationPluginGenericConsolinno::processDiscoveryObject(const Json::Value &responseObject, ThingDiscoveryInfo *info){
 	ThingDescriptor descriptor(
-		inverterThingClassId, 
+		genericConsolinnoConnectionThingClassId, 
 		QString::fromStdString(responseObject["manufacturer"].asString()), 
-		QString::fromStdString(responseObject["model"].asString())
+		QString::fromStdString(
+			"Model: " + responseObject["model"].asString() + 
+			", Interface: " + responseObject["interface"].asString() + 
+			", Slave: " + responseObject["slaveId"].asString())
 	);
 	ParamList params;
-	params << Param(inverterThingConnectionParamsParamTypeId, QVariant::fromValue((QString::fromStdString(responseObject["connectionParams"].asString()))));
+	params << Param(genericConsolinnoConnectionThingConnectionParamsParamTypeId, QVariant::fromValue((QString::fromStdString(responseObject["connectionParams"].asString()))));
 	descriptor.setParams(params);
 	info->addThingDescriptor(descriptor);
 }
@@ -92,10 +95,10 @@ void IntegrationPluginGenericConsolinno::setupThing(ThingSetupInfo *info)
     qCDebug(dcGenericConsolinno()) << "Setup" << thing << thing->params();
 	m_timer = hardwareManager()->pluginTimerManager()->registerTimer(1);
 
-    if (thing->thingClassId() == inverterThingClassId) {		
+    if (thing->thingClassId() == genericConsolinnoConnectionThingClassId) {
 
 		connect(m_timer, &PluginTimer::timeout, thing, [this, thing](){
-			QString connectionParams = thing->paramValue(inverterThingConnectionParamsParamTypeId).toString();
+			QString connectionParams = thing->paramValue(genericConsolinnoConnectionThingConnectionParamsParamTypeId).toString();
 			// qCDebug(dcGenericConsolinno()) << "connectionParams:" << connectionParams;
 			//Jsonrpc request to get currentData
 			HttpClient client(urlModbusRTU);
@@ -103,37 +106,59 @@ void IntegrationPluginGenericConsolinno::setupThing(ThingSetupInfo *info)
 			Json::Value params;
 			params["connectionParams"] = connectionParams.toStdString();
 			params["block"] = -1;
+			Things meterThings = myThings().filterByParentId(thing->id()).filterByThingClassId(meterThingClassId);
+
+			bool inverterDataFound = false;
+			bool meterDataFound = false;
+			
 			try {
 				//Save c.CallMethod("discoverAllDevices", params); responce to json object
 				Json::Value result = c.CallMethod("getCurrentData", params);
+					
 				//iterate on result assuming it is an array
 				for (Json::Value::ArrayIndex i = 0; i < result.size(); i++) {
 					//each element is a json object naming a modbus block
 					Json::Value block = result[i];
 					//iterate for key value pairs on block
 					for (auto const& key : block.getMemberNames()) {
-						//Check if key contains "Interfaces::pvinverter::", if not continue
-						if(key.find("Interfaces::pvinverter::") == std::string::npos){
-							continue;
-						}
-
-						//Extract stateName from key after "Interfaces::pvinverter::"
-						QString stateName = QString::fromStdString(key.substr(24));
-						//Get stateValue for variant type
-						QVariant stateValue = thing->stateValue(stateName);
-						//Get type from variant
-						if(stateValue.type() == QVariant::Double){
-							thing->setStateValue(stateName, block[key].asFloat());
-							qCDebug(dcGenericConsolinno()) << "stateValue:" << block[key].asFloat();
+						//Check if key contains "Interfaces::pvinverter::"
+						if(key.find("Interfaces::pvinverter::") != std::string::npos){
+							//Extract stateName from key after "Interfaces::pvinverter::"
+							QString stateName = QString::fromStdString(key.substr(24));
+							//Get stateValue for variant type
+							QVariant stateValue = thing->stateValue(stateName);
+							//Get type from variant
+							if(thing->hasState(stateName) && stateValue.type() == QVariant::Double){
+								thing->setStateValue(stateName, block[key].asFloat());
+								// qCDebug(dcGenericConsolinno()) << "stateValue:" << block[key].asFloat();
+							}
+							inverterDataFound = true;
+						} else if(key.find("Interfaces::meter::") != std::string::npos && !meterThings.isEmpty()){
+							//Extract stateName from key after "Interfaces::meter::"
+							QString stateName = QString::fromStdString(key.substr(19));
+							//Get stateValue for variant type
+							QVariant stateValue = meterThings.first()->stateValue(stateName);
+							//Get type from variant
+							if(meterThings.first()->hasState(stateName) && stateValue.type() == QVariant::Double){
+								meterThings.first()->setStateValue(stateName, block[key].asFloat());
+							}
+							meterDataFound = true;
 						}
 					}
 				}
 
-				thing->setStateValue(inverterConnectedStateTypeId, true);				
+				thing->setStateValue(genericConsolinnoConnectionConnectedStateTypeId, inverterDataFound);
+				
+
 			} catch (JsonRpcException &e) {
 				// std::cerr << e.what() << std::endl;
-				thing->setStateValue(inverterConnectedStateTypeId, false);
+				thing->setStateValue(genericConsolinnoConnectionConnectedStateTypeId, false);
 			}
+
+			if(!meterThings.isEmpty()){
+				meterThings.first()->setStateValue(meterConnectedStateTypeId, meterDataFound);
+			}
+
 		});
 		info->finish(Thing::ThingErrorNoError);    
         
@@ -142,8 +167,8 @@ void IntegrationPluginGenericConsolinno::setupThing(ThingSetupInfo *info)
         info->finish(Thing::ThingErrorNoError);
         Thing *parentThing = myThings().findById(thing->parentId());
         if (parentThing) {
-            thing->setStateValue(meterConnectedStateTypeId, parentThing->stateValue(inverterConnectedStateTypeId).toBool());
-        }
+            thing->setStateValue(meterConnectedStateTypeId, parentThing->stateValue(genericConsolinnoConnectionConnectedStateTypeId).toBool());
+        }		
     } else if (thing->thingClassId() == batteryThingClassId) {
         // Nothing to do here, we get all information from the inverter connection
         info->finish(Thing::ThingErrorNoError);
@@ -161,18 +186,18 @@ void IntegrationPluginGenericConsolinno::setupThing(ThingSetupInfo *info)
 
         Thing *parentThing = myThings().findById(thing->parentId());
         if (parentThing) {
-            thing->setStateValue(batteryConnectedStateTypeId, parentThing->stateValue(inverterConnectedStateTypeId).toBool());
+            thing->setStateValue(batteryConnectedStateTypeId, parentThing->stateValue(genericConsolinnoConnectionConnectedStateTypeId).toBool());
         }
     }
 }
 
 void IntegrationPluginGenericConsolinno::postSetupThing(Thing *thing)
 {
-    if (thing->thingClassId() == inverterThingClassId) {
+    if (thing->thingClassId() == genericConsolinnoConnectionThingClassId) {
         // Check if w have to set up a child meter for this inverter connection
         if (myThings().filterByParentId(thing->id()).filterByThingClassId(meterThingClassId).isEmpty()) {
             qCDebug(dcGenericConsolinno()) << "Setup new meter for" << thing;
-            emit autoThingsAppeared(ThingDescriptors() << ThingDescriptor(meterThingClassId, "Generic Consolinno Energy Meter", QString(), thing->id()));
+            emit autoThingsAppeared(ThingDescriptors() << ThingDescriptor(meterThingClassId, thing->name() + " meter", QString(), thing->id()));
         }
     }
 }
