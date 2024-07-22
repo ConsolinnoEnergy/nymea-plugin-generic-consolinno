2ce04d4e1e23d0a8f6559a74fee9091a766c6eda
Luis Reber <l.reber@consolinno.de>
2024-07-22 11:17:26 +0200

    Prepare for generator

--- genericconsolinno.pro
+++ genericconsolinno.pro
@@ -1,11 +1,46 @@
-include(plugins.pri)
+TEMPLATE = subdirs
 
-QT += network
+PLUGIN_DIRS = \
+    genericconsolinno      \
 
-SOURCES += \
-    integrationplugingenericconsolinno.cpp
+message(============================================)
+message("Qt version:" $$[QT_VERSION])
 
-HEADERS += \
-    integrationplugingenericconsolinno.h
+plugininfo.depends = FORCE
+for (entry, PLUGIN_DIRS):plugininfo.commands += test -d $${entry} || mkdir -p $${entry}; cd $${entry} && qmake -o Makefile $$PWD/$${entry}/$${entry}.pro && cd ..;
+for (entry, PLUGIN_DIRS):plugininfo.commands += make -C $${entry} plugininfo.h;
+QMAKE_EXTRA_TARGETS += plugininfo
 
-LIBS += -ljsonrpccpp-common -ljsonrpccpp-client -ljsoncpp
+# Translations:
+# make lupdate to update .ts files
+lupdate.depends = FORCE plugininfo
+for (entry, PLUGIN_DIRS):lupdate.commands += make -C $${entry} lupdate;
+QMAKE_EXTRA_TARGETS += lupdate
+
+# make lrelease to build .qm from .ts
+lrelease.depends = FORCE
+for (entry, PLUGIN_DIRS):lrelease.commands += lrelease $$files($$PWD/$${entry}/translations/*.ts, true);
+QMAKE_EXTRA_TARGETS += lrelease
+
+# For Qt-Creator's code model: Add CPATH to INCLUDEPATH explicitly
+INCLUDEPATH += $$(CPATH)
+
+message("Usage: qmake [srcdir] [WITH_PLUGINS=\"...\"] [WITHOUT_PLUGINS=\"...\"]")
+
+isEmpty(WITH_PLUGINS) {
+    PLUGINS = $${PLUGIN_DIRS}
+} else {
+    PLUGINS = $${WITH_PLUGINS}
+}
+PLUGINS-=$${WITHOUT_PLUGINS}
+
+message("Building plugins:")
+for(plugin, PLUGINS) {
+    exists($${plugin}) {
+        SUBDIRS*= $${plugin}
+        message("- $${plugin}")
+    } else {
+        error("Invalid plugin \"$${plugin}\".")
+    }
+}
+
---
+++ genericconsolinno/translations/7567e677-9586-42f8-af1f-050d5953edcc-en_US.ts
@@ -0,0 +1,240 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>genericConsolinno</name>
+    <message>
+        <source>Battery capacity kWh</source>
+        <extracomment>The name of the ParamType (ThingClass: battery, Type: settings, ID: {3a4e551a-0858-4c7b-8e48-0ce4948b4e23})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Battery critical</source>
+        <extracomment>The name of the StateType ({7840d69d-f3df-4f6a-9d61-1d20b62828ab}) of ThingClass battery</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Battery level</source>
+        <extracomment>The name of the StateType ({188aea63-ed1f-461e-b4db-ffd236490e59}) of ThingClass battery</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Capacity</source>
+        <extracomment>The name of the StateType ({1d9f1b6f-3f20-454d-a81d-9c3575d88bd9}) of ThingClass battery</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <extracomment>The name of the StateType ({d3691be0-b8c1-4731-8c1a-b8ebb221385d}) of ThingClass battery
+----------
+The name of the StateType ({f10dc25b-c418-40a6-ba98-15a9c35639a3}) of ThingClass meter
+----------
+The name of the StateType ({c53d368e-c0c9-4b76-8e7d-6b4d0d690721}) of ThingClass inverter
+----------
+The name of the StateType ({d8c99359-7c58-454c-a3a8-c014c0c69b65}) of ThingClass genericConsolinnoConnection</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connection parameters</source>
+        <extracomment>The name of the ParamType (ThingClass: genericConsolinnoConnection, Type: thing, ID: {584de5e5-e4ef-48dc-a16f-03b1045b0b68})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Current power phase A</source>
+        <extracomment>The name of the StateType ({78364c00-1420-49b4-9788-017aa4aabda8}) of ThingClass meter
+----------
+The name of the StateType ({95666638-990d-40a6-ac46-329201913624}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Current power phase B</source>
+        <extracomment>The name of the StateType ({12b8042d-f192-4503-9b74-1d047e3f39f9}) of ThingClass meter
+----------
+The name of the StateType ({db358abe-db04-4cab-93c7-c5a072e70d06}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Current power phase C</source>
+        <extracomment>The name of the StateType ({0094a817-1f07-4d5f-8ee7-aa5c38de63e7}) of ThingClass meter
+----------
+The name of the StateType ({c97644fa-7340-4ac9-b8dd-51f62fbbaf63}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>DC Voltage PV 1</source>
+        <extracomment>The name of the StateType ({8b84dfd4-6357-41ff-959f-326e09554b9c}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>DC Voltage PV 2</source>
+        <extracomment>The name of the StateType ({c62943f6-be25-4454-a664-332ef66c30ca}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Energy feed to battery today</source>
+        <extracomment>The name of the StateType ({b2fe3d81-90f8-4218-b179-3cbe917c1e16}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Energy feed to battery total</source>
+        <extracomment>The name of the StateType ({582722e9-cb9b-436a-8af8-ebc57cb101ed}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Frequency</source>
+        <extracomment>The name of the StateType ({4736e499-3da2-4936-a28c-8dcfe0c29eb9}) of ThingClass meter
+----------
+The name of the StateType ({d409f439-0ee2-4011-aa9f-30d879701bf5}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Generic Consolinno</source>
+        <extracomment>The name of the vendor ({39b1d35d-23c5-41a3-a44c-5f8b986b4d54})
+----------
+The name of the plugin genericConsolinno ({7567e677-9586-42f8-af1f-050d5953edcc})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Generic Consolinno Battery</source>
+        <extracomment>The name of the ThingClass ({26107c27-297a-44bb-9ade-ea0539335e6c})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Generic Consolinno Energy Meter</source>
+        <extracomment>The name of the ThingClass ({52d8fc9b-65a3-46e3-adc5-6199db31d0a2})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Generic Consolinno Gateway</source>
+        <extracomment>The name of the ThingClass ({79132b53-c55e-4a92-b0af-11bccb33bebb})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Generic Consolinno Inverter</source>
+        <extracomment>The name of the ThingClass ({1fdfde5d-ae6a-48e0-a52f-88f28fa781da})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Grid Export Limit (percent of inverter nominal power)</source>
+        <extracomment>The name of the ParamType (ThingClass: inverter, ActionType: setExportLimit, ID: {85ee134b-c2d0-49fd-89aa-7edfe80f40c1})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Instantaneous power</source>
+        <extracomment>The name of the StateType ({e820efab-8a46-4c48-a14f-95964cb44f11}) of ThingClass battery</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Inverter Nominal power (watt)</source>
+        <extracomment>The name of the ParamType (ThingClass: inverter, ActionType: setExportLimit, ID: {0f897ca3-a942-4d93-bad9-0fa62037d529})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Nominal Power AC</source>
+        <extracomment>The name of the StateType ({93fa06ff-dc56-4064-a055-2e2ca5eaba9f}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>PV DC power</source>
+        <extracomment>The name of the StateType ({8620a41d-bf05-4b02-a706-ef9a13e0a381}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Phase A current</source>
+        <extracomment>The name of the StateType ({275779a0-1e1c-4f5b-924e-445205e46e15}) of ThingClass meter
+----------
+The name of the StateType ({251e6ca5-dea9-4272-bcd1-1d17c4740c08}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Phase A voltage</source>
+        <extracomment>The name of the StateType ({58d291db-1c02-44b3-b95f-8787cd89e243}) of ThingClass meter
+----------
+The name of the StateType ({0f071455-060b-4201-8b47-4d44ebe4d66c}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Phase B current</source>
+        <extracomment>The name of the StateType ({7966f32b-8602-47c5-bfe4-062847d065ae}) of ThingClass meter
+----------
+The name of the StateType ({deef2adc-53b2-47bd-aa21-0c268a96eb3d}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Phase B voltage</source>
+        <extracomment>The name of the StateType ({ab349c54-614b-416e-8914-fc6b4ded8cd4}) of ThingClass meter
+----------
+The name of the StateType ({054c6fa3-4311-4261-a2d6-f6988baa6b59}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Phase C current</source>
+        <extracomment>The name of the StateType ({40e71a55-5d87-4257-9529-432a6380e8b6}) of ThingClass meter
+----------
+The name of the StateType ({a9664e99-36c4-42b5-8732-694a0e66f686}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Phase C voltage</source>
+        <extracomment>The name of the StateType ({2114593d-8213-4629-aea7-7c3284f8898a}) of ThingClass meter
+----------
+The name of the StateType ({4d76d895-5fdc-45ad-a7b1-ee9aff5997bd}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Set Grid export limit</source>
+        <extracomment>The name of the ActionType ({60bca780-fd59-4b21-b8a8-f3e895813802}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Temperature</source>
+        <extracomment>The name of the StateType ({dd52b2ca-41bd-4e20-9cd3-d3bdb0dbab43}) of ThingClass battery</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Today energy produced</source>
+        <extracomment>The name of the StateType ({afb7e3dd-041f-417c-a02f-dc85bc992f87}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Total AC power</source>
+        <extracomment>The name of the StateType ({6fc19b9f-b051-4c2e-9e19-d3530d1e98e2}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Total energy produced</source>
+        <extracomment>The name of the StateType ({98ad322c-98b7-442f-a780-4639f32e6f9b}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Total real energy exported</source>
+        <extracomment>The name of the StateType ({dbbd2ab8-4b19-436a-ab98-f3d81ddc0dfb}) of ThingClass meter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Total real energy imported</source>
+        <extracomment>The name of the StateType ({150d3ee6-5fc4-4aa0-aa17-68b75f3d69ca}) of ThingClass meter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Total real power</source>
+        <extracomment>The name of the StateType ({31e8cbd5-20d1-4fd9-972b-25eb44d2710f}) of ThingClass meter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Voltage</source>
+        <extracomment>The name of the StateType ({b84985a0-c259-471c-ad4b-cbaaf06cc82c}) of ThingClass battery</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>actual Grid export power limit</source>
+        <extracomment>The name of the StateType ({4401e7c7-8450-4c1e-9224-ae46388d8ee4}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>actual PV power limit</source>
+        <extracomment>The name of the StateType ({d12c9da6-e6c3-4522-9d41-372739778f7f}) of ThingClass inverter</extracomment>
+        <translation></translation>
+    </message>
+</context>
+</TS>
